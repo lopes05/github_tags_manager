f275c05acf331fcc314258308a393ca889f9a754
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2019-08-26 00:04:41 -0300

    add Procfile

--- teste_labhacker/settings.py
+++ teste_labhacker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,5 @@ LOGIN_REDIRECT_URL = 'home'
 
 SOCIAL_AUTH_CUSTOM_GITHUB_KEY = os.environ.get("SOCIAL_AUTH_CUSTOM_GITHUB_KEY")
 SOCIAL_AUTH_CUSTOM_GITHUB_SECRET = os.environ.get("SOCIAL_AUTH_CUSTOM_GITHUB_SECRET")
+
+django_heroku.settings(locals())
